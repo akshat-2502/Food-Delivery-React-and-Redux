4d9edb825eefa7366ddcd5b9e5e251a656ac5909
akshat-2502 <khulbeakshat@gmail.com>
2025-06-18 12:10:45 +0530

    for toggle button

---
+++ src/components/SwitchButton.jsx
@@ -0,0 +1,25 @@
+import React, { useContext } from "react";
+import { dataContext } from "../context/UserContext";
+
+const SwitchButton = () => {
+  let { darkMode, setDarkMode } = useContext(dataContext);
+  return (
+    <div
+      onClick={() => setDarkMode(!darkMode)}
+      onCl
+      className={`w-[50px] h-[22px] transition-all duration-600 bg-gray-700 flex border-1 border-green-500 overflow-hidden rounded-full ${
+        darkMode ? "bg-green-500" : null
+      }`}
+    >
+      <span
+        className={`h-[20px] w-[20px] rounded-full transition-all duration-600 bg-white overflow-hidden ${
+          darkMode ? "ml-7" : null
+        }`}
+      ></span>
+    </div>
+  );
+};
+
+export default SwitchButton;
+
+// done
